226dd0db0c142ad1aeea56d4007004b1542cd186
Max Kapur <maxkapur@gmail.com>
2022-05-03 17:55:14 +0900

    split slides into 8- and 20-minute versions; translations

--- compile.sh
+++ compile.sh
@@ -9,7 +9,7 @@ sed '3 c\
 \\newif\\ifen\\enfalse' paper.tex | pdflatex -no-parse-first-line -jobname=paper-ko
 
 # Slides
-cd ./slides
+cd ./slides_20min
 sed '3 c\
 \\newif\\ifen\\entrue' slides.tex  | pdflatex -no-parse-first-line -jobname="slides-en"
 sed '3 c\
@@ -18,3 +18,16 @@ sed '3 c\
 \\newif\\ifen\\entrue' slides.tex  | pdflatex -no-parse-first-line -jobname="slides-en"
 sed '3 c\
 \\newif\\ifen\\enfalse' slides.tex | pdflatex -no-parse-first-line -jobname="slides-ko"
+cd ..
+
+cd ./slides_08min
+sed '3 c\
+\\newif\\ifen\\entrue' slides.tex  | pdflatex -no-parse-first-line -jobname="slides-en"
+sed '3 c\
+\\newif\\ifen\\enfalse' slides.tex | pdflatex -no-parse-first-line -jobname="slides-ko"
+sed '3 c\
+\\newif\\ifen\\entrue' slides.tex  | pdflatex -no-parse-first-line -jobname="slides-en"
+sed '3 c\
+\\newif\\ifen\\enfalse' slides.tex | pdflatex -no-parse-first-line -jobname="slides-ko"
+cd ..
+
